87a386f553b5ded581c3c2869f69b27c43b47426
Laura Fernandes <lauqfernandes@gmail.com>
2022-03-28 22:12:23 -0300

    http e swagger implementados

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import cors from "cors"
+import swaggerUI from "swagger-ui-express"
+import fs from "fs"
 
-const app = express();
+const app = express()
 const port = process.env.PORT || 4000
 
-app.use(cors()); //Habilita o CORS  Cross Origin Resource Sharing
-app.use(express.urlencoded({extended:true})); // habilita url com acentos
-app.use(express.json()); // habilita o parse de conteúdo JSON
-app.disable('x-powered-by'); // remove por segurança
+app.use(cors()) //Habilita o CORS-Cross Origin Resource Sharing
+app.use(express.urlencoded({extended:true})) //habilita url com acentos
+app.use(express.json()) // Habilita o parse de conteúdo JSON
+//app.disable('x-powered-by') //Remove o Powered by por segurança
+//import rotasCategoria from './routes/categoria.js'
 
 //Rota padrão
 app.get('/', (req, res) => {
     res.status(200).json({
-        mensagem:'API 100% Funcional', versao:'1.0.0'
+        mensagem: 'API 100% Funcional', versao: '1.0.0'
     })
 })
 
-app.listen(port, function () {
+//Definimos a rota das categorias
+//app.use('/categorias', rotasCategoria)
+
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(JSON.parse(fs.readFileSync('./src/swagger/swagger_output.json'))))
+
+app.listen(port, function() {
     console.log(`Servidor rodando na porta ${port}`)
 })
--- src/routes/categorias.js
+++ src/routes/categorias.js
@@ -1,10 +1,22 @@
 import express from "express";
 import res from "express/lib/response";
 import { connectToDatabase } from "../util/mongodb.js";
+import { check, validationResult } from "express-validator"
 
 const router = express.Router();
 const nomeCollection = "categorias";
 const { db, ObjectId } = await connectToDatabase();
+
+//Validações das categorias
+
+const validaCategoria = [
+    check('nome').not().notEmpty().trim().withMessage('O nome é obrigatório!')
+        .isLength({min:3}).withMessage('Nome muito curto. Min 3 letras')
+        .isLength({max:100}).withMessage('Nome muito longo. Max 200 letras')
+        .isAlpha('pt-br', {ignore: ''}).withMessage('Nome deve conter apenas letras'),
+    check('status').default(true).not().isString().withMessage('status não pode ser texto')
+        .not().isInt().withMessage('O status não pode ser número'),
+]
 
 /**
  * GET / categorias
@@ -24,5 +36,98 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+/**
+ * GET / categorias/id
+ */
+router.get("/:id", async (req, res) => {
+    try{
+        db.collection(nomeCollection).find(
+            {"_id": {$eq: ObjectId(req.params.id)}}).toArray(
+                (err, docs) => {
+                    if(err) {res.status(400).json(err)}
+                    else{res.status(200).json(docs)}
+                }
+        )
+    }
+    catch(err) {
+        res.status(500).json({"error":err.message})
+    }
+})
+
+/**
+ * GET / categorias/nome
+ */
+ router.get("/nome/:nome", async (req, res) => {
+    try{
+        db.collection(nomeCollection).find(
+            {"nome": {$regex: req.params.nome, $options: "i"}}).toArray(
+                (err, docs) => {
+                    if(err) {res.status(400).json(err)}
+                    else{res.status(200).json(docs)}
+                }
+        )
+    }
+    catch(err) {
+        res.status(500).json({"error":err.message})
+    }
+})
+
+/**
+ * POST / categorias/nome
+ * inclui nova categoria
+ */
+router.post("/", validaCategoria, async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    else{
+        await db.collection(nomeCollection)
+        .insertOne(req, body)
+        .then(result => res.status(201).send(result))
+        .catch(err => res.status(400).json(err))
+    }
+})
+
+/**
+ * DELETE / categorias
+ * apaga categoria
+ */
+router.delete("/:id", async (req, res) => {
+    await db.collection(nomeCollection)
+    .deleteOne({"_id": {$eq: ObjectId(req.params.id)}})
+    .then(result => res.status(202).send(result))
+    .catch(err => res.status(400).json(err))
+})
+
+/**
+ * PUT / categorias/nome
+ * apaga categoria
+ */
+router.put("/", validaCategoria, async(req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    else{
+        const categoriaDados = req.body
+        await db.collection(nomeCollection)
+        .updateOne({"_id": {$eq: ObjectId(req.params.id)}},
+        {
+            $set:{
+                nome: categoriaDados.nome,
+                status: categoriaDados.status
+            }
+        }
+        )
+        .then(result => res.status(202).send(result))
+        .catch(err => res.status(400).json(err))
+    }
+})
 
 export default router
